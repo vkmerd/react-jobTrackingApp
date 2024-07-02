dc1d2a189c905ff69b42414659ea4fd43cba99b8
Mert Öztürk <mert32139@gmail.com>
2024-07-02 03:01:54 +0300

    iş takip uygulaması projesi kuruldu, template ve bağımlılıklar inşaa edilmeye başlandı

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Login from './templates/login/Login'
+ 
+export default function App() {
+
+  return (
+    <>
+     <div className="loginContainer">
+      <Login />
+     </div>
+    </>
+  )
+}
+
---
+++ src/templates/login/Login.jsx
@@ -0,0 +1,12 @@
+
+export default function Login(){
+    return(
+        <>
+            <div className="loginComponentContainer">
+                <div className="loginForm"></div>
+                <div className="loginImage">
+                </div>
+            </div>
+        </>
+    )
+}
